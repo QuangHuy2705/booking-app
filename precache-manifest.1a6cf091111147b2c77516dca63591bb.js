207fa93e2e37beb7b6f598337ba0600cbc425473
QuangHuy <ntquanghuy2705@gmail.com>
2019-07-13 00:30:28 +0300

    Updates

---
+++ precache-manifest.1a6cf091111147b2c77516dca63591bb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd99f4d45d81f00f28932ada6800581e",
+    "url": "/booking-app/index.html"
+  },
+  {
+    "revision": "39f20573c80fb86c662e",
+    "url": "/booking-app/static/css/5.ee77bfac.chunk.css"
+  },
+  {
+    "revision": "b30a04cdb28c3896817d",
+    "url": "/booking-app/static/css/main.d99ace9b.chunk.css"
+  },
+  {
+    "revision": "def10780efa10a80c9e5",
+    "url": "/booking-app/static/js/0.d02ce5cb.chunk.js"
+  },
+  {
+    "revision": "bd0353dd43927a517902",
+    "url": "/booking-app/static/js/3.b589aa41.chunk.js"
+  },
+  {
+    "revision": "c7db7a1f9a9dbd9147eb",
+    "url": "/booking-app/static/js/4.34d44e02.chunk.js"
+  },
+  {
+    "revision": "39f20573c80fb86c662e",
+    "url": "/booking-app/static/js/5.e0bffe5a.chunk.js"
+  },
+  {
+    "revision": "c44d5aec7a3d9ed9dd35",
+    "url": "/booking-app/static/js/6.faffd033.chunk.js"
+  },
+  {
+    "revision": "8f879484014a48e1e17b",
+    "url": "/booking-app/static/js/7.6cf9c08d.chunk.js"
+  },
+  {
+    "revision": "8c2cd8c5e12138b67209",
+    "url": "/booking-app/static/js/8.32499264.chunk.js"
+  },
+  {
+    "revision": "61b86fccca94801e3fb8",
+    "url": "/booking-app/static/js/9.038eb75c.chunk.js"
+  },
+  {
+    "revision": "b30a04cdb28c3896817d",
+    "url": "/booking-app/static/js/main.c75f7a89.chunk.js"
+  },
+  {
+    "revision": "fa7cf81d22a2e9c99eaf",
+    "url": "/booking-app/static/js/runtime~main.32044b34.js"
+  },
+  {
+    "revision": "c7746a1208859f51df4cc1dd1ec00f03",
+    "url": "/booking-app/static/media/Eight1.c7746a12.jpg"
+  },
+  {
+    "revision": "525553e684027209a9cf842ed654a067",
+    "url": "/booking-app/static/media/Five1.525553e6.jpg"
+  },
+  {
+    "revision": "548a03be212621f5c5de1026c9bbdf56",
+    "url": "/booking-app/static/media/Four1.548a03be.jpg"
+  },
+  {
+    "revision": "76938a97b6537a43cb438abbee52009d",
+    "url": "/booking-app/static/media/One1.76938a97.jpg"
+  },
+  {
+    "revision": "e6bde60fc4043e921b7d32ed335abe65",
+    "url": "/booking-app/static/media/Seven1.e6bde60f.jpg"
+  },
+  {
+    "revision": "97189d7f9d0a0b414fe290a617a8d829",
+    "url": "/booking-app/static/media/Six1.97189d7f.jpg"
+  },
+  {
+    "revision": "9bffe2b0b403a35567346c2b0c556696",
+    "url": "/booking-app/static/media/Three1.9bffe2b0.jpg"
+  },
+  {
+    "revision": "4daddd4fcbe7d80bb6872eddcfe5da45",
+    "url": "/booking-app/static/media/Two1.4daddd4f.jpg"
+  },
+  {
+    "revision": "8f1e2d7935b27a19d956a1dd97426198",
+    "url": "/booking-app/static/media/appetizer.8f1e2d79.jpg"
+  },
+  {
+    "revision": "1fac1b9a1b6e8d4777a4cf8818960b9d",
+    "url": "/booking-app/static/media/back-customers.1fac1b9a.jpg"
+  },
+  {
+    "revision": "ef46f149a3d3475be8a80cb9786036fd",
+    "url": "/booking-app/static/media/background-hero.ef46f149.jpeg"
+  },
+  {
+    "revision": "b56b435116658f4521f1a9a16d482501",
+    "url": "/booking-app/static/media/customer-1.b56b4351.jpg"
+  },
+  {
+    "revision": "9b6c894aa65ca6695c00f356c02555bb",
+    "url": "/booking-app/static/media/customer-2.9b6c894a.jpg"
+  },
+  {
+    "revision": "ff8c09d162705f54636450395be5ca21",
+    "url": "/booking-app/static/media/drinks.ff8c09d1.jpg"
+  },
+  {
+    "revision": "e02324d8d6d132c0ec693d62fd742b05",
+    "url": "/booking-app/static/media/fried-rice1.e02324d8.jpg"
+  },
+  {
+    "revision": "1fff6e0f3e187628ea7ec4c374f85aea",
+    "url": "/booking-app/static/media/fries.1fff6e0f.jpg"
+  },
+  {
+    "revision": "a67b734e8793ead6e82022d0d370730a",
+    "url": "/booking-app/static/media/loader.a67b734e.gif"
+  },
+  {
+    "revision": "93f9c92588a66b37be5b7ec4bc950858",
+    "url": "/booking-app/static/media/logo-white.93f9c925.png"
+  },
+  {
+    "revision": "6762543a5c1deed3472b676309560f77",
+    "url": "/booking-app/static/media/logo.6762543a.png"
+  },
+  {
+    "revision": "f08cc740ebaf10537f7df5b80d2606d4",
+    "url": "/booking-app/static/media/main-dish.f08cc740.jpg"
+  },
+  {
+    "revision": "880a0f02bbc0330836d2bec1851e5dbf",
+    "url": "/booking-app/static/media/noodles-and-pho.880a0f02.jpg"
+  },
+  {
+    "revision": "f059f45179cc2c4b9727dca6bcca2221",
+    "url": "/booking-app/static/media/rolls.f059f451.jpg"
+  },
+  {
+    "revision": "99e70130740bcc37b92539e1c6d298b7",
+    "url": "/booking-app/static/media/side-dish.99e70130.jpg"
+  }
+]);
